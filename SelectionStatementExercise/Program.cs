34b32485682c9b30d74e4b1706c4ec36984c3df0
2305 <npjt2305@yahoo.com>
2022-10-24 17:21:00 -0400

    finished exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace SelectionStatementExercise
+﻿using System;
+
+namespace SelectionStatementExercise
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low.");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high.");
+            }
+            else
+            {
+                Console.WriteLine("Correct!");
+            }
+
         }
     }
+    
 }
